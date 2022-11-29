24db6a6bd818456d995bb51911fdfe32c8799d70
Chris Edgington <chris.edgington@zinc.io>
2022-11-29 12:09:17 -0500

    Fix single page issues with top gap and get rid of avatar image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -363,32 +363,8 @@ export function Header() {
           <>
             <div
               ref={avatarRef}
-              className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"
+              className="order-last"
             />
-            <Container
-              className="top-0 order-last -mb-3 pt-3"
-              style={{ position: 'var(--header-position)' }}
-            >
-              <div
-                className="top-[var(--avatar-top,theme(spacing.3))] w-full"
-                style={{ position: 'var(--header-inner-position)' }}
-              >
-                <div className="relative">
-                  <AvatarContainer
-                    className="absolute left-0 top-3 origin-left transition-opacity"
-                    style={{
-                      opacity: 'var(--avatar-border-opacity, 0)',
-                      transform: 'var(--avatar-border-transform)',
-                    }}
-                  />
-                  <Avatar
-                    large
-                    className="block h-16 w-16 origin-left"
-                    style={{ transform: 'var(--avatar-image-transform)' }}
-                  />
-                </div>
-              </div>
-            </Container>
           </>
         )}
         <div
@@ -401,13 +377,13 @@ export function Header() {
             style={{ position: 'var(--header-inner-position)' }}
           >
             <div className="relative flex gap-4">
-              <div className="flex flex-1">
+              {/* <div className="flex flex-1">
                 {!isHomePage && (
                   <AvatarContainer>
                     <Avatar />
                   </AvatarContainer>
                 )}
-              </div>
+              </div> */}
               <div className="flex flex-1 justify-end md:justify-center">
                 <MobileNavigation className="pointer-events-auto md:hidden" />
                 <DesktopNavigation className="pointer-events-auto hidden md:block" />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -266,7 +266,7 @@ export default function Home({ articles }) {
           content="I’m Chris, a software architect and problem solver based in rural Indiana."
         />
       </Head>
-      <Container className="mt-16 sm:mt-32">
+      <Container className="mt-2 sm:mt-4">
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
